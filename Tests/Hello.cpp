6266f088b79bd42d7122a2b17686acd1bce7da2a
Yahya <yahyanaja@ymail.com>
2018-10-11 13:28:33 -0700

    debugging

--- Tests/Hello.cpp
+++ Tests/Hello.cpp
@@ -55,7 +55,8 @@ receive(m_ptr_Float);
         // Print(" ");
         // Print(o_ptr);
         // Print("\n");
-       // *o_ptr = o_ptr_Float + m_ptr_Float; // elem ; // + m_ptr_Float;
+       // *o_ptr =
+        Float t = o_ptr_Float + m_ptr_Float; // elem ; // + m_ptr_Float;
        // m_ptr = m_ptr + 16;
        // o_ptr = o_ptr + 16;
    // End
